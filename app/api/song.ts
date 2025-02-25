8fbec852cc1bab43cfda85a196e6542714410622
khantu12 <khantu12@gmail.com>
2025-02-25 22:50:42 +0200

    feat: add id and fix file eslint

--- app/api/song.ts
+++ app/api/song.ts
@@ -1,17 +1,18 @@
-import prisma from "@/lib/prisma"
+import prisma from '@/lib/prisma';
 
 export const getTopSongs = async () => {
   return await prisma.song.findMany({
     take: 10,
     select: {
+      id: true,
       title: true,
       image: true,
       artist: {
         select: {
           id: true,
           name: true,
-        }
-      }
-    }
-  })
-}
+        },
+      },
+    },
+  });
+};
