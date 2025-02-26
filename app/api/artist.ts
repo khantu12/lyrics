3327ca487fb2ceab9d7e58721de298ce265c6d54
Yulian Prodanov <ilu100@abv.bg>
2025-02-26 10:15:00 +0200

    add artist func

---
+++ app/api/artist.ts
@@ -0,0 +1,30 @@
+import prisma from '@/lib/prisma';
+
+export const getArtist = async (id: number) => {
+  return await prisma.artist.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      name: true,
+      image: true,
+      description: true,
+      realName: true,
+      songs: {
+        select: {
+          id: true,
+          title: true,
+          image: true,
+        },
+      },
+      albums: {
+        select: {
+          id: true,
+          title: true,
+          image: true,
+        },
+      },
+    },
+  });
+};
